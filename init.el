eeba052ca06a0739d6373c7c3fab5d0b33b0e024
Connor Kelly <connor.david.kelly@gmail.com>
2022-06-08 16:35:20 -0500

    - upped kill-ring-max - down/upcase-region - added to things_to_elisp-ify.txt

--- init.el
+++ init.el
@@ -158,3 +158,5 @@
 ;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
